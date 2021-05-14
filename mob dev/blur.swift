8c7014b7fcea67cd952f5024f97864a4dd1d8b7a
Тимофей Веретнов <timofejveretnov@TimofejdeMacBook-Pro.local>
2021-05-14 21:38:49 +0700

    blur

---
+++ mob dev/blur.swift
@@ -0,0 +1,104 @@
+//
+//  blur.swift
+//  mob dev
+//
+//  Created by Тимофей Веретнов on 07.05.2021.
+//  Copyright © 2021 Пользователь. All rights reserved.
+
+//
+//  GaussianBlur.swift
+//
+//  Created by Aryaman Sharda on 9/12/20.
+//  Copyright © 2020 com.DigitalBunker. All rights reserved.
+//
+import Foundation
+import UIKit
+import SwiftImage
+//
+//let blurImage = GaussianBlur.createBlurredImage(radius: 5, image: UIImage(named: "ggwp.jpg")!)
+//let image = UIImage(named: "ggwp.jpg")
+//let originImage = Image<RGBA<UInt8>>(uiImage: image )
+//var unSharpImage : Image<RGBA<UInt8>>
+//let width = image.width
+//let height = image.height
+//for x in 0...height{
+//    for y in 0...width{
+//        unSharpImage[x,y].red = originImage[x,y].red - blurImage[x,y].red
+//        unSharpImage[x,y].green = originImage[x,y].green - blurImage[x,y].green
+//        unSharpImage[x,y].blue = originImage[x,y].blue - blurImage[x,y].blue
+////        if unSharpImage[x,y].red < 0 unSharpImage[x,y].red = 0
+////        if unSharpImage[x,y].green < 0 unSharpImage[x,y].green = 0
+////        if unSharpImage[x,y].blue < 0 unSharpImage[x,y].blue = 0
+//    }
+//}
+
+final class GaussianBlur {
+    
+    static func createBlurredImage(radius: Int, image: UIImage) -> Image<RGBA<UInt8>> {
+        let inputImage = Image<RGBA<UInt8>>(uiImage: image)
+        var outputImage = Image<RGBA<UInt8>>(uiImage: image)
+
+        
+        let sigma = max(Double(radius / 2), 1)
+
+        
+        let kernelWidth = (2 * radius) + 1
+
+        
+        var kernel = Array(repeating: Array(repeating: 0.0, count: kernelWidth), count: kernelWidth)
+        var sum = 0.0
+
+        
+        for x in -radius...radius {
+            for y in -radius...radius {
+                let exponentNumerator = Double(-(x * x + y * y))
+                let exponentDenominator = (2 * sigma * sigma)
+
+                let eExpression = pow(M_E, exponentNumerator / exponentDenominator)
+                let kernelValue = (eExpression / (2 * Double.pi * sigma * sigma))
+
+                
+                kernel[x + radius][y + radius] = kernelValue
+                sum += kernelValue
+            }
+        }
+
+        
+        for x in 0..<kernelWidth {
+            for y in 0..<kernelWidth {
+                kernel[x][y] /= sum
+            }
+        }
+
+        
+        for x in radius..<(inputImage.width - radius) {
+            for y in radius..<(inputImage.height - radius) {
+
+                var redValue = 0.0
+                var greenValue = 0.0
+                var blueValue = 0.0
+
+                
+                for kernelX in -radius...radius {
+                    for kernelY in -radius...radius {
+
+                       
+                        let kernelValue = kernel[kernelX + radius][kernelY + radius]
+
+                       
+                        redValue += Double(inputImage[x - kernelX, y - kernelY].red) * kernelValue
+                        greenValue += Double(inputImage[x - kernelX, y - kernelY].green) * kernelValue
+                        blueValue += Double(inputImage[x - kernelX, y - kernelY].blue) * kernelValue
+                    }
+                }
+
+                
+                outputImage[x,y].red = UInt8(redValue)
+                outputImage[x,y].green = UInt8((greenValue))
+                outputImage[x,y].blue = UInt8(blueValue)
+            }
+        }
+
+        return outputImage
+    }
+}
